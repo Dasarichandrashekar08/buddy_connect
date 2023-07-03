1a5f18cdd0b06a0968f56e1b27884669c3e08475
Chandra Shekar D <dchandrashekar1999@gmail.com>
2023-07-03 17:58:33 +0530

    removed db url

--- chatbackend/database/connect.js
+++ chatbackend/database/connect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongo_url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iolasks.mongodb.net/buddyconnect?retryWrites=true&w=majority`;
+const mongo_url = ''
 mongoose.connect(mongo_url, {
     useNewUrlParser: true,
     useUnifiedTopology: true
